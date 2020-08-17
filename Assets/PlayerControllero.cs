206cd138e8a618a402797a8004bc8fa890ae7c08
ShiroHaru99 <48929700+ShiroHaru99@users.noreply.github.com>
2020-08-18 00:17:11 +0200

    Commit 1

---
+++ Assets/PlayerControllero.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerControllero : MonoBehaviour
+{
+    public float maxVelocidad = 7f;
+    public float velocidad = 3f;
+
+    private Rigidbody2D Cuerpo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cuerpo = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        Cuerpo.AddForce(Vector2.right * velocidad * h);
+
+        if (Cuerpo.velocity.x > maxVelocidad)
+        {
+            Cuerpo.velocity = new Vector2(maxVelocidad, Cuerpo.velocity.y);
+        }
+        if (Cuerpo.velocity.x < -maxVelocidad)
+        {
+            Cuerpo.velocity = new Vector2(-maxVelocidad, Cuerpo.velocity.y);
+        }
+    }
+}
