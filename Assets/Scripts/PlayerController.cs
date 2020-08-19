d75d3744cc20e37d309177f4ad51345d13a86ea0
Iván <48927047+saurasmaker@users.noreply.github.com>
2020-08-19 05:01:37 +0200

    Mecánicas y animaciones del personaje hechas

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     //Attributes
     public float maxSpeed = 15f;
     public float speed = 10f;
+    public float jumpPower = 6.5f;
 
     private Rigidbody2D rb2d;
     private Animator animator;
@@ -27,8 +28,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (Input.GetKeyDown(KeyCode.Space)) isJumping = true;
         
-        
+        CheckState(rb2d.velocity.x, rb2d.velocity.y);
+        SetAnimations();
     }
 
     private void FixedUpdate()
@@ -37,14 +41,11 @@ public class PlayerController : MonoBehaviour
         float y = Input.GetAxis("Vertical");
 
         rb2d.AddForce(Vector2.right * speed * x);
-        rb2d.AddForce(Vector2.up * speed * y * 5);
+        //rb2d.AddForce(Vector2.up * speed * y * 5);
 
-        float limitedSpeed = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
-        rb2d.velocity = new Vector2(limitedSpeed, rb2d.velocity.y);
-
-        CheckState(rb2d.velocity.x, rb2d.velocity.y);
-        SetAnimations();
-
+        float limitedSpeedX = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
+        //float limitedSpeedY = Mathf.Clamp(rb2d.velocity.y, -maxSpeed, maxSpeed);
+        rb2d.velocity = new Vector2(limitedSpeedX, rb2d.velocity.y/*limitedSpeedY*/);
 
         Debug.Log("Velocidad Horizontal: " + rb2d.velocity.x + "\n Velocidad Vertical: " + rb2d.velocity.y + "\n Saltando: " + isJumping + "\n Cayendo: " + isFalling);
 
@@ -68,8 +69,13 @@ public class PlayerController : MonoBehaviour
         if (y < 0) isFalling = true;
         else isFalling = false;
 
-        if (x < 0) transform.localScale = new Vector3(-1f, 1f, 1f);
-        else if (x > 0) transform.localScale = new Vector3(1f, 1f, 1f); ;
+        if (isJumping && isGrounded) { 
+            rb2d.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            isJumping = false;
+        }
+
+        if (x < -0.1) transform.localScale = new Vector3(-1f, 1f, 1f);
+        else if (x > 0.1) transform.localScale = new Vector3(1f, 1f, 1f); ;
 
         return;
     }
@@ -81,6 +87,14 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("Grounded", isGrounded);
         animator.SetBool("Jumping", isJumping);
         animator.SetBool("Falling", isFalling);
+
+        return;
+    }
+
+    void OnBecameInvisible()
+    {
+
+        transform.position = new Vector3(-12, -3, -1);
 
         return;
     }
