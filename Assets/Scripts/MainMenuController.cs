f38949f43f31b6210d533ba2c553fff83f1dc76f
Iván <48927047+saurasmaker@users.noreply.github.com>
2020-08-20 05:50:50 +0200

    Menú inicial implementado
    Cambios de escena y cerrar juego implementado correctamente.

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void SwitchScene(string sceneName)
+    {
+        if (sceneName.Equals("Exit"))
+        {
+            Application.Quit();
+            return;
+        }
+
+        SceneManager.LoadScene(sceneName);
+
+        return;
+    }
+    
+}
