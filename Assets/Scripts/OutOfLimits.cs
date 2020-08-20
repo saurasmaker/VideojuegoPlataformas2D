c6f8b2539a4be3f4a43f82664c3dd245c40e59a3
Iván <48927047+saurasmaker@users.noreply.github.com>
2020-08-20 06:42:31 +0200

    Implementación Fuera de los Límites
    Se ha implementado la siguiente funcionalidad:
    
    En el momento en el que el personaje se sale de los margenes de la pantalla aparece al otro lado de esta manteniendo velocidad y posicionen el eje Y y Z.

---
+++ Assets/Scripts/OutOfLimits.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfLimits : MonoBehaviour
+{
+    public EdgeCollider2D ec2dMirror;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.gameObject.transform.position = new Vector3(ec2dMirror.transform.position.x, collision.gameObject.transform.position.y, collision.gameObject.transform.position.z); 
+        }
+    }
+}
---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+    bool paused;
+    Canvas canvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GetComponentInParent<Canvas>();
+        Resume();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+            canvas.enabled = paused;
+            Time.timeScale = (paused) ? 0 : 1;
+        }
+
+    }
+
+    public void Resume()
+    {
+        canvas.enabled = paused = false;   
+        Time.timeScale = 1;
+
+        return;
+    }
+
+    public void LoadScene(string nameScene)
+    {
+        SceneManager.LoadScene(nameScene);
+
+        return;
+    }
+
+}
